2575a9bc38a953c88812fea3653c41f9fbef7f33
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-02-08 16:18:58 -0400

    - se creo el rest api de los archivos llamado files. - - Se creo la logica de los endpoint para mostrar las imagenes y guardar las imagenes. - - Se creo la logica para mostrar las imagenes en el FilesServices. -

---
+++ src/files/files.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
+import { BadRequestException } from '@nestjs/common/exceptions';
+
+
+@Injectable()
+export class FilesService {
+
+    getStaticProductImage(imageName: string){
+        const path = join(__dirname, '../../static/products', imageName);
+
+        if(!existsSync(path))
+            throw new BadRequestException(`No product found with image ${imageName}`);
+
+        return path;
+    }
+
+}
