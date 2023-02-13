eac95c27a57da98729d927791c7b89f175647273
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-02-13 16:06:49 -0400

    - Se agrego el AuthModule al app.module.ts. -

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { FilesModule } from './files/files.module';
 import { Server } from 'http';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -34,7 +35,9 @@ import { join } from 'path';
 
     SeedModule,
 
-    FilesModule
+    FilesModule,
+
+    AuthModule
   ],
 })
 export class AppModule {}
