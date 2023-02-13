da8e76505750c132e8fdeee9904bbe69d07c2920
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-02-13 16:09:04 -0400

    - se creo el api rest auth. - - Se creo el metodo register y login. - - Se crearon los metoods private,private2 y private 3 para ejemplo del las autorizaciones de las rutas -

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,74 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto, LoginUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from './entities/user.entity';
+import { RawHeaders, GetUser, RoleProtected, Auth } from './decorators';
+import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { ValidRoles } from './interfaces';
+
+
+@Controller('auth')
+export class AuthController {
+
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.createUser(createUserDto);
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.loginUser(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute(
+  @Req() request: Express.Request,
+  @GetUser() user: User,
+  @GetUser('email') userEmail: string,
+  @RawHeaders() rawHeaders: string[],
+  @Headers() headers: IncomingHttpHeaders
+  ){
+    console.log(request)
+
+    return{
+      ok: true,
+      message: 'Hola Mundo Private',
+      user,
+      userEmail,
+      rawHeaders,
+      headers
+    }
+
+  }
+
+  @Get('private2')
+  // @SetMetadata('roles', ['admin','super-user'])
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ){
+    return {
+      ok: true,
+      user
+    }
+  }
+
+  @Get('private3')
+  @Auth( ValidRoles.admin, ValidRoles.superUser )
+  privateRoute3(
+    @GetUser() user: User
+  ){
+    return {
+      ok: true,
+      user
+    }
+  }
+
+
+}
