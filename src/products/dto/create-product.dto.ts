60dee3ba2e1260875927cda196682b6111aa9eca
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-01 11:27:19 -0400

    - se agrego el class validator IsDate al dto de create-product.  -

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+import { IsArray, IsDate, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
 
 
 export class CreateProductDto {
