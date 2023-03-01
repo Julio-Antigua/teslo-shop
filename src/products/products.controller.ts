1eba9a3449f4ed049c0ae907d18f05b2c2e2162e
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-01 11:15:43 -0400

    - se agregaron las validaciones de autenticacion y autorizacion en el controlador de productos. -

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,9 +3,13 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth, GetUser } from 'src/auth/decorators';
+import { ValidRoles } from '../auth/interfaces/valid-roles';
+import { User } from 'src/auth/entities/user.entity';
 
 @Controller('products')
 export class ProductsController {
+
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
@@ -17,20 +21,28 @@ export class ProductsController {
   findOne(@Param('term') term: string) {
     return this.productsService.findOnePlain(term);
   }
+
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  @Auth(ValidRoles.admin)
+  create(
+    @Body() createProductDto: CreateProductDto,
+    @GetUser() user: User
+  ) {
+    return this.productsService.create(createProductDto, user);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(
     @Param('id', ParseUUIDPipe) id: string,
-    @Body() updateProductDto: UpdateProductDto
+    @Body() updateProductDto: UpdateProductDto,
+    @GetUser() user: User
   ) {
-    return this.productsService.update( id, updateProductDto);
+    return this.productsService.update( id, updateProductDto, user);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
