3f5607716abc3f8e92a50700b73435ce26722562
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-01 11:09:14 -0400

    - Se agrego la relacion One to Many del producto con el usuario. - - Se agregaron las propiedades de la interface de auditoria. -

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,7 +1,10 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Auditory } from 'src/common/interfaces/auditory.interface';
+import { Product } from 'src/products/entities';
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
-export class User {
+export class User implements Auditory {
+    
 
     @PrimaryGeneratedColumn('uuid')
     id: string;
@@ -36,15 +39,59 @@ export class User {
     })
     roles: string[];
 
+    @OneToMany(
+        () => Product,
+        ( product ) => product.user
+    )
+    product: Product;
+
+    @Column({
+        type: 'text',
+        default: ''
+    })
+    creationUser: string;
+
+    @Column({
+        type: 'timestamp',
+        default: new Date()
+    })
+    creationDate: Date;
+
+    @Column({
+        type: 'text',
+        default: ''
+    })
+    modificationUser: string;
+    
+    @Column({
+        type: 'timestamp',
+        default: new Date()
+    })
+    modificationDate: Date;
+
+    
     @BeforeInsert()
     checkFieldsBeforeInsert(){
         this.email = this.email.toLocaleLowerCase().trim();
     }
-
+    
     @BeforeUpdate()
     checkFieldsBeforeUpdate(){
         this.checkFieldsBeforeInsert();
     }
+    
+    @BeforeInsert()
+    AuditoryInsert(){
+        this.creationDate = new Date();
+        this.creationUser = this.fullName;
+        this.modificationDate = new Date();
+        this.modificationUser = this.email;
+    }
 
+    @BeforeUpdate()
+    AuditoryDateUpdate(){
+        this.modificationUser = this.email;
+        this.modificationDate = new Date();
+    }
 
 }
