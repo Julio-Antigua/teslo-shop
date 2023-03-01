5dbf93c2c21d54d21c3a1a69e1c439943cbfc70c
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-01 11:39:19 -0400

    - se agrego la importacion de AuthModule y el providers AuthService. -

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { ProductsModule } from '../products/products.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   controllers: [SeedController],
-  imports: [ProductsModule],
-  providers: [SeedService]
+  imports: [
+    ProductsModule,
+    AuthModule
+  ],
+  providers: [SeedService, AuthService]
 })
 export class SeedModule {}
